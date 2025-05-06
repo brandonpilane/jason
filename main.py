2cc546e5cf2b8d6f1179f5fffa622a057e582bbf
Brandon <BrandonPilane@gmail.com>
2025-05-06 16:59:18 +0200

    Code: read from a JSON file

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ data = {'Hello': 'World!', 'Status': 'Init', 'active': True}
 # write to a JSON file
 with open('data.json', 'w') as f:
     json.dump(data, f, indent=4)  # pretty print
+
+# read from a JSON file
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+print(data)
